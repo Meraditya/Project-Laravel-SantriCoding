25e94733c925cc9ead60fd0c1ed10b25f27d7dfe
raditya <meradityawahyu@gmail.com>
2025-05-24 18:26:11 +0700

    Insert data ke dalam Database

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -8,6 +8,14 @@ use App\Models\Product;
 //import return type View
 use Illuminate\View\View;
 
+
+//import return type redirectResponse
+use Illuminate\Http\RedirectResponse;
+
+//import Http Request
+use Illuminate\Http\Request;
+
+
 class ProductController extends Controller
 {
     /**
